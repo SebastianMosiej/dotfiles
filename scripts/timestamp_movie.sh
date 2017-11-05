a08d5b00155f804fa4f757747cfc46bf7183be5f
Sebastian Mosiej <sebastian.mosiej@wp.pl>
2017-11-05 21:02:50 +0100

    Normalizing movies for concat uses the same pix_fmt

--- scripts/timestamp_movie.sh
+++ scripts/timestamp_movie.sh
@@ -3,27 +3,27 @@
 
 TARGET_VCODEC='libx264'
 TARGET_ACODEC='aac'
-
-TIMESTAMP_FORMAT="drawtext=fontfile=Verdana.ttf:fontsize=20:expansion=normal:text='%{pts\\:gmtime\\:$seconds}':r=30:x=(w-tw)-40:y=h-(2*lh):fontcolor=white:box=1:boxcolor=0x000000@1"
+OUTPUT_FORMAT="-hide_banner -c:a $TARGET_ACODEC -c:v $TARGET_VCODEC -ac 2 -ar 48000\
+              -r 30 -strict -2 -s 1280x720 -profile:v high422 -pix_fmt yuvj422p -video_track_timescale 60000 -y"
 
 which ffprobe > /dev/null || (echo "Lack of 'ffprobe'. Install it";exit)
 which ffmpeg > /dev/null || (echo "Lack of 'ffmpeg'. Install it";exit)
+stat Verdana.ttf &>> /dev/null || (echo "Lack of Verdana.ttf in the same folder.";exit)
 
 if [[ ! -e out ]];then
   echo 'Creating out directiory'
   mkdir out
 fi
 
-function get_file_vcodec() {
-  vcodec=`ffprobe -v error -select_streams v:0 -show_entries stream=codec_name \
-    -of default=noprint_wrappers=1:nokey=1 $1`
-}
 function get_file_creation_seconds() {
   local TZ=""
   time=`ffprobe $1 2>&1 | grep creation_time -m1 | cut -d : -f 2- | cut -d' ' -f 2-`
   if [[ -z $time ]]; then
     time=`stat -c %y $i`
   else
+    if [[ ${time:26:1} -eq 'Z' ]]; then
+      time=${time::-1}
+    fi
     TZ="GMT"
   fi
   seconds=`date -d "$time $TZ" +%s`
@@ -33,18 +33,29 @@ function get_file_creation_seconds() {
 function convert_mov_to_mp4() {
   local filename=`basename $1`
   local extension="${filename##*.}"
+  filename="${filename%.*}" 
+  get_file_creation_seconds $1
+  filename=$time"_${filename%.*}"
+
+  echo "Recoding file $1 - start at $seconds" &>>log
+  ffmpeg -i $1\
+    -vf "setdar=16:9,drawtext=fontfile=Verdana.ttf:fontsize=20:expansion=normal:text='%{pts\\:localtime\\:$seconds}':r=30:x=(w-tw)-40:y=h-(2*lh):fontcolor=white:box=1:boxcolor=0x000000@1"\
+    $OUTPUT_FORMAT out/$filename.mp4 &>>log
+}
+
+function convert_avi_to_mp4() {
+  local filename=`basename $1`
+  local extension="${filename##*.}"
   filename="${filename%.*}"
 
   get_file_creation_seconds $1
   filename=$time"_${filename%.*}"
 
   echo "Recoding file $1 - start at $seconds" &>>log
-  ffmpeg -i $1 -acodec $TARGET_ACODEC -vcodec $TARGET_VCODEC\
-    -vf "scale=1280:720,setdar=16:9,drawtext=fontfile=Verdana.ttf:fontsize=20:expansion=normal:text='%{pts\\:localtime\\:$seconds}':r=30:x=(w-tw)-40:y=h-(2*lh):fontcolor=white:box=1:boxcolor=0x000000@1"\
-    -r 25 -y -strict experimental out/$filename.mp4 &>>log
-    #-vf "scale=1280:720,setdar=16:9" \
+  ffmpeg -i $1\
+    -vf "setdar=16:9,drawtext=fontfile=Verdana.ttf:fontsize=20:expansion=normal:text='%{pts\\:localtime\\:$seconds}':r=30:x=(w-tw)-40:y=h-(2*lh):fontcolor=white:box=1:boxcolor=0x000000@1"\
+    $OUTPUT_FORMAT out/$filename.mp4 &>>log
 }
-
 
 function add_timestamp(){
   local file=$1
@@ -59,30 +70,46 @@ function add_timestamp(){
   echo "Adding timestamp $1" &>>log
   ffmpeg -i $file\
     -vf "drawtext=fontfile=Verdana.ttf:fontsize=20:expansion=normal:text='%{pts\\:localtime\\:$seconds}':r=30:x=(w-tw)-40:y=h-(2*lh):fontcolor=white:box=1:boxcolor=0x000000@1"\
-    -strict -2 -y out/$filename &>>log
+    $OUTPUT_FORMAT out/$filename &>>log
 }
 
-mov_files=(`find -P . -maxdepth 1 -xdev \
-  -iname '*.MOV' -o -iname '*.MTS' -o -iname '*.AVI' | sort`)
+mov_files=(`find -P . -maxdepth 1 -xdev -iname '*.MOV' -o -iname '*.MTS' | sort`)
+avi_files=(`find -P . -maxdepth 1 -xdev -iname '*.AVI' | sort`)
 movie_files=(`find -P . -maxdepth 1 -xdev -iname '*.MP4' | sort`)
 
 index=1
 files_count=${#movie_files[@]}
 echo "" > log
+echo "Adding timestamp to MP4 movies"
 for i in "${movie_files[@]}"
 do
   echo -ne " [$index/$files_count] Processing file $i\r"
   add_timestamp $i
   ((index++))
 done
+echo ""
+
+if [[ ${#avi_files[@]} -gt 0 ]]; then
+  echo
+  echo "Converting AVI files to MP4"
+  index=1
+  files_count=${#avi_files[@]}
+  for i in  "${avi_files[@]}"
+  do
+    echo -ne " [$index/$files_count] Converting file $i\r"
+    convert_avi_to_mp4 $i 
+    ((index++))
+  done
+fi
 
 if [[ ${#mov_files[@]} -gt 0 ]]; then
+  echo ""
   echo "Converting MOV file to MP4"
   index=1
   files_count=${#mov_files[@]}
   for i in  "${mov_files[@]}"
   do
-  echo -ne " [$index/$files_count] Converting file $i\r"
+    echo -ne " [$index/$files_count] Converting file $i\r"
     convert_mov_to_mp4 $i 
     ((index++))
   done
