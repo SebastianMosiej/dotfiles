2845297b859f77d5bac2b533534b0b17f872afab
Sebastian Mosiej <sebastian.mosiej@siili.com>
2022-08-23 16:51:18 +0200

    Update scripts

--- scripts/laptop_docking.sh
+++ scripts/laptop_docking.sh
@@ -1,35 +1,46 @@
 #!/bin/bash 
 #-vx
 
+BUILTIN_DISPLAY=eDP-1
+LEFT_DISPLAY=HDMI-1
+RIGHT_DISPLAY=DP-2
+
 function toogle_monitors()
 {
-  connected_output=(`xrandr -d :0 | grep -w connected | cut -d' ' -f1 | grep -v eDP1`)
+  connected_output=(`xrandr -d :0 | grep -w connected | cut -d' ' -f1 | grep -v $BUILTIN_DISPLAY`)
   #remove modes for disconnected displays
+  disconnected1=(`xrandr -d :0 | grep -w disconnected | cut -d' ' -f1-2`)
   disconnected=(`xrandr -d :0 | egrep 'disconnected [0-9].*' | cut -d' ' -f1`)
-  logger -t laptop_docking "Detected ${#disconnected[@]} disconnected but still present monitors "
+  logger -t laptop_docking "Detected ${#connected_output[@]} connected and  ${#disconnected[@]} disconnected displays, but still present monitors"
+  logger -t laptop_docking "Connected '${connected_output[@]}' and disconected '${disconnected[@]}'"
+  logger -t laptop_docking "Disconected '${disconnected1[@]}'"
   if [[ ${#disconnected[@]} -eq 1 ]]; then
     logger -t laptop_docking "Disconnecting ${disconnected[0]} display"
     xrandr -d :0 --output ${disconnected[0]} --off
   fi
   if [[ ${#connected_output[@]} -eq 1 ]]; then
-    echo "One monitor - beside laptop - found"
     logger -t laptop_docking "One monitor - beside laptop - found"
     case "${connected_output[0]}" in 
-      "HDMI2" ) #laptop is in docking station
-        xrandr -d :0 --output HDMI2 --auto --primary --pos 0x0
-        xrandr -d :0 --output eDP1 --auto --left-of HDMI2
+      "$LEFT_DISPLAY" ) #laptop is in docking station
+        xrandr -d :0 --output $LEFT_DISPLAY --auto --primary --pos 0x0
+        xrandr -d :0 --output $BUILTIN_DISPLAY --auto --left-of $LEFT_DISPLAY
         ;;
       *) 
-        xrandr --output eDP1 --auto --primary --pos 0x0
-        xrandr --output ${connected_output[0]} --auto --right-of eDP1
+        xrandr --output $BUILTIN_DISPLAY --auto --primary --pos 0x0
+        xrandr --output ${connected_output[0]} --auto --right-of $BUILTIN_DISPLAY
         ;;
     esac
+  elif [[ ${#connected_output[@]} -eq 2 ]]; then
+        xrandr -d :0 --output $RIGHT_DISPLAY --auto --primary --pos 0x0
+        xrandr -d :0 --output $LEFT_DISPLAY --auto --left-of $RIGHT_DISPLAY
+        xrandr -d :0 --output $BUILTIN_DISPLAY --auto --right-of $RIGHT_DISPLAY
   elif [[ ${#connected_output[@]} -eq 0 ]]; then
     logger -t laptop_docking "Only laptop monitor found"
-    echo "Only laptop monitor found"
-    xrandr -d :0 --output HDMI2 --off
-    xrandr -d :0 --output eDP1 --primary
+    xrandr -d :0 --output $LEFT_DISPLAY --off
+    xrandr -d :0 --output $RIGHT_DISPLAY --off
+    xrandr -d :0 --output $BUILTIN_DISPLAY --auto --primary --pos 0x0
   fi
+  logger -t laptop_docking "Monitor toggling completed"
 }
 
 function restart_i3()
@@ -38,7 +49,7 @@ function restart_i3()
   toogle_monitors
   killall -SIGCONT i3
   i3-msg restart
-  feh --bg-fill /usr/share/images/wallpapers/forest/Lush_Mossy_Forest.jpg
+  feh -r $HOME/.wallpaper --bg-max -z -D 300 &
 }
 
 # if DISPLAY variable isn't set
