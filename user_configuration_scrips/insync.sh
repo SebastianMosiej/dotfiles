c7f551ac9b341586124fdc3b3936747de5586e0e
Sebatian Mosiej <sebastian.mosiej@wp.pl>
2025-06-24 13:16:30 +0200

    setup: add insync with apt repo

--- user_configuration_scrips/insync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -qO - https://d2t3ff60b2tol4.cloudfront.net/services@insynchq.com.gpg.key | apt-key add -
-echo "deb http://apt.insynchq.com/`lsb_release -is | tr '[:upper:]' ''[:lower:]` `lsb_release -sc` non-free contrib" >> /etc/apt/sources.list
-apt-get update
-apt-get install insync -yq
