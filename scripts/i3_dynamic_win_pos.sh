26c16736d909af9e96c464270b366d236552251c
Sebatian Mosiej <sebastian.mosiej@wp.pl>
2025-08-07 10:12:33 +0200

    i3: use window class to set ppt position

--- scripts/i3_dynamic_win_pos.sh
+++ scripts/i3_dynamic_win_pos.sh
@@ -2,10 +2,15 @@
 # Get workspace/screen dimensions
 SCREEN_WIDTH=$(xrandr --current | grep '*' | head -1 | awk '{print $1}' | cut -d 'x' -f1)
 SCREEN_HEIGHT=$(xrandr --current | grep '*' | head -1 | awk '{print $1}' | cut -d 'x' -f2)
+WINDOW_CLASS=$3
 
 # Calculate 60% from left and 5% from top
 POS_X=$((SCREEN_WIDTH * ${1:-60} / 100))
 POS_Y=$((SCREEN_HEIGHT * ${2:-5} / 100))
 
 # Move the window
-i3-msg "move position ${POS_X}px ${POS_Y}px"
+MESSAGE="move position ${POS_X}px ${POS_Y}px"
+if [ -n $WINDOW_CLASS ]; then
+  MESSAGE="[class=\"$WINDOW_CLASS\"] $MESSAGE"
+fi
+i3-msg "$MESSAGE"
